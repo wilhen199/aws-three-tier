89bc1fb0dbb1254ef3b15ce538f1f1168e70413e
Wilhen Figueredo <wilhen199@gmail.com>
2026-07-03 17:55:36 -0500

    feat: add module compute

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
 module "network" {
   source = "./modules/network"
 
-  aws_region   = var.aws_region
-  project_name = var.project_name
-  environment  = var.environment
-  vpc_cidr     = var.vpc_cidr
+  aws_region         = var.aws_region
+  project_name       = var.project_name
+  environment        = var.environment
+  vpc_cidr           = var.vpc_cidr
+  availability_zones = var.availability_zones
+  #public_subnets      = var.public_subnets
+  #private_subnets_web = var.private_subnets_web
+}
+
+module "compute" {
+  source              = "./modules/compute"
+  vpc_id              = module.network.vpc_id
+  aws_region          = var.aws_region
+  project_name        = var.project_name
+  environment         = var.environment
+  public_subnets      = module.network.public_subnets
+  private_subnets_web = module.network.private_subnets_web
+
 }
