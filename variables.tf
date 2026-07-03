b00abef070b7db000bbd486168a8bde7ed563d86
Wilhen Figueredo <wilhen199@gmail.com>
2026-07-03 17:57:12 -0500

    feat: add AZs variables

--- variables.tf
+++ variables.tf
@@ -18,3 +18,8 @@ variable "vpc_cidr" {
   type        = string
   description = "VPC CIDR block"
 }
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
