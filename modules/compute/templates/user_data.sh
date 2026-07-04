03f08153f8387f4830d0ce1b1bbc14ac37082a58
Wilhen Figueredo <wilhen199@gmail.com>
2026-07-03 19:39:52 -0500

    feat: add Test Website

--- modules/compute/templates/user_data.sh
+++ modules/compute/templates/user_data.sh
@@ -3,3 +3,7 @@ dnf update -y
 dnf install -y nginx
 systemctl start nginx
 systemctl enable nginx
+
+# Test Website
+echo "<h1> Nginx Server  $(hostname -f) </h1>" > /var/www/html/index.html
+echo "<h2> Deployed via Terraform </h2>" >> /var/www/html/index.html
