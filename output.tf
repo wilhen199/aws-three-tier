70113b68cbcccedcc98da0976c132df2d8b745ac
Wilhen Figueredo <wilhen199@gmail.com>
2026-07-04 19:21:15 -0500

    feat: expose db_endpoint and db_secret_arn fron database module

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "alb_dns_name" {
   value       = "http://${module.compute.alb_dns_name}"
   description = "The DNS name of the ALB"
 }
+
+output "db_endpoint" {
+  value       = module.database.db_endpoint
+  description = "Database endpoint"
+}
+
+output "db_secret_arn" {
+  value       = module.database.db_secret_arn
+  description = "ARN del secreto de la BD en Secrets Manager"
+}
